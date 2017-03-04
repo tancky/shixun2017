7abb142cba72089ced44531f6d0cc458b73a464d
tancky <546349822@qq.com>
2017-03-04 13:20:19 +0800

    test0

---
+++ entry.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Rayr Lee on 2017/3/4.
+ */
+
+var add=require("./module.js")  //添加模块
+
+document.write("hello world");
+
+document.write(add);
---
+++ module.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Rayr Lee on 2017/3/4.
+ */
+
+
+module.exports="it works from module.js."
