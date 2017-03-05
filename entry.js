257f3e716f0eedeb94920437435630792af8b4f4
tancky <546349822@qq.com>
2017-03-05 12:35:18 +0800

    test01

--- entry.js
+++ entry.js
@@ -2,7 +2,10 @@
  * Created by Rayr Lee on 2017/3/4.
  */
 
-var add=require("./module.js")  //添加模块
+var add =require("./module.js")  //添加模块
+var $ =require("jquery");
+
+console.log(jq);
 
 document.write("hello world");
 
